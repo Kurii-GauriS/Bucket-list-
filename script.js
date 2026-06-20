30fe6a127e7032b699ed5f23078c96d1f5c14d01
Kurii-GauriS <gaurisangare933@gmail.com>
2026-06-20 20:13:06 +0530

    <h1>🖤 My SKZ Wishlist 💗</h1>
    <div id="wishlistContainer">
    </div>
    
    <script src="script.js"></script>

--- script.js
+++ script.js
@@ -78,3 +78,109 @@ captureBtn.onclick = ()=>{
     document.body.appendChild(img);
 
 }
+function addTask() {
+
+    let input =
+    document.getElementById("bucketInput");
+
+    let task = input.value;
+
+    if(task === "") return;
+
+    let dreams =
+    JSON.parse(
+        localStorage.getItem("dreams")
+    ) || [];
+
+    dreams.push(task);
+
+    localStorage.setItem(
+        "dreams",
+        JSON.stringify(dreams)
+    );
+
+    input.value="";
+
+    alert("Dream Added! ✨");
+}
+function loadWishlist(){
+
+    let container =
+    document.getElementById(
+        "wishlistContainer"
+    );
+
+    if(!container) return;
+
+    let dreams =
+    JSON.parse(
+        localStorage.getItem("dreams")
+    ) || [];
+
+    dreams.forEach(dream => {
+
+        let card =
+        document.createElement("div");
+
+        card.classList.add("wish-card");
+
+        card.innerHTML =
+     `
+     🌸 ${dream}
+     <button class="deleteBtn">
+     ❌
+     </button>
+     `;
+
+        container.appendChild(card);
+
+    });
+}
+
+loadWishlist();
+function loadWishlist(){
+
+    let container =
+    document.getElementById(
+        "wishlistContainer"
+    );
+
+    if(!container) return;
+
+    let dreams =
+    JSON.parse(
+        localStorage.getItem("dreams")
+    ) || [];
+
+    dreams.forEach(dream => {
+
+        let card =
+        document.createElement("div");
+
+        card.classList.add("wish-card");
+
+        card.innerHTML =
+        "🌸 " + dream;
+
+        container.appendChild(card);
+
+    });
+}
+
+loadWishlist();
+card.querySelector(".deleteBtn")
+.addEventListener("click",()=>{
+
+    dreams =
+    dreams.filter(
+        item => item !== dream
+    );
+
+    localStorage.setItem(
+        "dreams",
+        JSON.stringify(dreams)
+    );
+
+    location.reload();
+
+});
